faa4574485a551492166e96265756b21dc77ec6c
Varga Csaba <vcsfrl@gmail.com>
2024-07-26 10:04:02 +0300

    Introduce TextOutputWriter and refactor accordingly.

--- logger/writer.go
+++ logger/writer.go
@@ -24,16 +24,14 @@ func (t *TextOutputWriter) Write(logMessage LogMessage) {
 	// We could panic on error, but that would be a bit too harsh.
 	// We could also log the error, with the standard library logger, but that would be
 	// a logger in the logger (having the same flaw).
-	_, _ = t.writer.Write(
-		[]byte(
-			fmt.Sprintf(
-				"%s :: %s :: %s :: %s :: %v \n",
-				logMessage.Timestamp.Format(time.RFC3339),
-				logMessage.Severity,
-				logMessage.Content,
-				strings.TrimLeft(fmt.Sprint(logMessage.Attributes), "map"),
-				logMessage.Tags,
-			),
+	_, _ = t.writer.Write([]byte(
+		fmt.Sprintf(
+			"%s :: %s :: %s :: %s :: %v \n",
+			logMessage.Timestamp.Format(time.RFC3339),
+			logMessage.Severity,
+			logMessage.Content,
+			strings.TrimLeft(fmt.Sprint(logMessage.Attributes), "map"),
+			logMessage.Tags,
 		),
-	)
+	))
 }
