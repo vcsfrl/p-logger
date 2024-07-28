b167a21cdbc65b8b1f19f541c890a06c93400b92
Varga Csaba <vcsfrl@gmail.com>
2024-07-28 07:47:22 +0300

    Support default tags.

---
+++ logger/logger_levels.go
@@ -0,0 +1,33 @@
+package logger
+
+// LevelLogger is a logger that logs messages with a specific severity level.
+// It is a wrapper around the Logger struct, ad adds some utility methods.
+type LevelLogger struct {
+	*Logger
+}
+
+func NewLevelLogger(logger *Logger) *LevelLogger {
+	return &LevelLogger{Logger: logger}
+}
+
+func (l *LevelLogger) Info(message string) {
+	l.Log(SeverityInfo, Message{Content: message})
+}
+
+func (l *LevelLogger) Debug(message string) {
+	l.Log(SeverityDebug, Message{Content: message})
+}
+
+func (l *LevelLogger) Warn(message string) {
+	l.Log(SeverityWarning, Message{Content: message})
+}
+
+func (l *LevelLogger) Error(message string) {
+	l.Log(SeverityError, Message{Content: message})
+}
+
+func (l *LevelLogger) Transaction(id string, attributes map[string]string) {
+	message := Message{Content: "Transaction", Attributes: attributes}
+	message.Attributes["transaction_id"] = id
+	l.Log(SeverityDefault, message)
+}
---
+++ logger/logger_levels_test.go
@@ -0,0 +1,55 @@
+package logger
+
+import (
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+	"testing"
+)
+
+func TestLevelLogger(t *testing.T) {
+	gunit.Run(new(LevelLoggerFixture), t, gunit.Options.AllSequential())
+}
+
+type LevelLoggerFixture struct {
+	*gunit.Fixture
+	memoryWriter *MemoryWriter
+	logger       *LevelLogger
+}
+
+func (f *LevelLoggerFixture) Setup() {
+	f.memoryWriter = new(MemoryWriter)
+	f.logger = NewLevelLogger(NewLogger(&TextOutputWriter{writer: f.memoryWriter}))
+	f.logger.DefaultTags = []string{"tag1", "tag2"}
+	// Mock the time.Now function
+	f.logger.now = mockNow()
+}
+
+func (f *LevelLoggerFixture) Teardown() {
+	f.memoryWriter.Reset()
+	_ = f.logger.Close()
+}
+
+func (f *LevelLoggerFixture) TestLevels() {
+	f.logger.Info("test")
+	f.So(f.memoryWriter.String(), should.Equal, "2024-05-01T03:12:03Z :: INFO :: test :: [] :: [tag1 tag2] \n")
+}
+
+func (f *LevelLoggerFixture) TestDebug() {
+	f.logger.Debug("test")
+	f.So(f.memoryWriter.String(), should.Equal, "2024-05-01T03:12:03Z :: DEBUG :: test :: [] :: [tag1 tag2] \n")
+}
+
+func (f *LevelLoggerFixture) TestWarn() {
+	f.logger.Warn("test")
+	f.So(f.memoryWriter.String(), should.Equal, "2024-05-01T03:12:03Z :: WARN :: test :: [] :: [tag1 tag2] \n")
+}
+
+func (f *LevelLoggerFixture) TestError() {
+	f.logger.Error("test")
+	f.So(f.memoryWriter.String(), should.Equal, "2024-05-01T03:12:03Z :: ERROR :: test :: [] :: [tag1 tag2] \n")
+}
+
+func (f *LevelLoggerFixture) TestTransaction() {
+	f.logger.Transaction("123", map[string]string{"key": "value"})
+	f.So(f.memoryWriter.String(), should.Equal, "2024-05-01T03:12:03Z :: INFO :: Transaction :: [key:value transaction_id:123] :: [tag1 tag2] \n")
+}
